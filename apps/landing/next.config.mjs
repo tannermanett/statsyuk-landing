38faf5bc26c53124dc4897ef683189d97c27e3bf
Tanner Manett <t.manett@gmail.com>
2025-09-22 13:41:40 -0400

    Revert "redo"
    This reverts commit a858449516c3d16078554e52518b96c0f6e4aab9.

--- apps/landing/next.config.mjs
+++ apps/landing/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/landing",
   eslint: {
     ignoreDuringBuilds: true,
   },
--- apps/landing/src/app/sitemap.ts
+++ apps/landing/src/app/sitemap.ts
@@ -7,14 +7,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const headersList = await headers();
   let domain = headersList.get("host") as string;
   let protocol = "https";
+  const basePath = "/landing";
 
   return [
     {
-      url: `${protocol}://${domain}`,
+      url: `${protocol}://${domain}${basePath}`,
       lastModified: new Date(),
     },
     ...allPosts.map((post) => ({
-      url: `${protocol}://${domain}/blog/${post.slug}`,
+      url: `${protocol}://${domain}${basePath}/blog/${post.slug}`,
       lastModified: new Date(),
     })),
   ];
